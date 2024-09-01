5b49b92bd23229708b6070d01eedf32755eee1cd
Proklink <danil.ogorodnikov.2000@mail.ru>
2024-09-01 15:28:29 +0300

    start

--- scripts/runner.js
+++ scripts/runner.js
@@ -170,6 +170,18 @@ Runner.spriteDefinition = {
     TREX: {x: 848, y: 2},
     STAR: {x: 645, y: 2}
   },
+  // HDPI: {
+  //   CACTUS_LARGE: {x: 652, y: 2},
+  //   CACTUS_SMALL: {x: 446, y: 2},
+  //   CLOUD: {x: 166, y: 2},
+  //   HORIZON: {x: 2, y: 104},
+  //   MOON: {x: 954, y: 2},
+  //   PTERODACTYL: {x: 260, y: 2},
+  //   RESTART: {x: 2, y: 2},
+  //   TEXT_SPRITE: {x: 1294, y: 2},
+  //   TREX: {x: 1678, y: 2},
+  //   STAR: {x: 1276, y: 2}
+  // }
   HDPI: {
     CACTUS_LARGE: {x: 652, y: 2},
     CACTUS_SMALL: {x: 446, y: 2},
@@ -182,18 +194,6 @@ Runner.spriteDefinition = {
     TREX: {x: 1678, y: 2},
     STAR: {x: 1276, y: 2}
   }
-  // HDPI: {
-  //   CACTUS_LARGE: {x: 652, y: 2},
-  //   CACTUS_SMALL: {x: 446, y: 2},
-  //   CLOUD: {x: 166, y: 2},
-  //   HORIZON: {x: 2, y: 131},
-  //   MOON: {x: 954, y: 2},
-  //   PTERODACTYL: {x: 260, y: 2},
-  //   RESTART: {x: 2, y: 2},
-  //   TEXT_SPRITE: {x: 1294, y: 2},
-  //   TREX: {x: 1708, y: 7},
-  //   STAR: {x: 1276, y: 2}
-  // }
 };
 
 
